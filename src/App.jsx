71b61277c53f9ce6c14f2397567ade5aa1fd5529
SoleneMarie <solene.plassart@gmail.com>
2024-08-19 17:06:33 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Title from "./components/Title";
+
+function App() {
+  return (
+    <>
+      <section className="mainBlock">
+        <Header Title="The Job Board" />
+      </section>
+    </>
+  );
+}
+
+export default App;
