b6db6ad8de7ae60b2d8f3198c531c1a574f7cfca
SoleneMarie <solene.plassart@gmail.com>
2024-08-19 17:40:13 +0200

    new

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,18 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      <section className="mainBlock">
-        <Header name="The Job Board" />
-        <main>
-          <section className="allJobs">
-            <Job
-              className="oneJob"
-              title="Full Time Sales Associate - Sydney Boutique"
-              contractType="CDI"
-              country="Australie"
-              city="Sydney"
-            />
-          </section>
-        </main>
-      </section>
+      <Header name="The Job Board" />
+      <main>
+        <section className="allJobs">
+          <Job
+            className="oneJob"
+            title="Full Time Sales Associate - Sydney Boutique"
+            contractType="CDI"
+            country="Australie"
+            city="Sydney"
+          />
+        </section>
+      </main>
     </>
   );
 }
